430fea0627adaf5b224bf89e4c9379d4cf6292e1
Nabil Babu <nabil.babu20@gmail.com>
2020-09-29 11:24:57 -0400

    Initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text sceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel; 
+   
+    void Update()
+    {
+        switch(Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                //sceneLabel.text = "Landscape Left";
+                LivesLabel.rectTransform.anchoredPosition = new Vector3(383.0f,-105.0f,0f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector3(-378.0f,-105.0f,0f);
+                break;
+            case DeviceOrientation.LandscapeRight:
+                //sceneLabel.text = "Landscape Right";
+                LivesLabel.rectTransform.anchoredPosition = new Vector3(246.0f,-105.0f,0f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector3(-498.0f,-105.0f,0f);
+                break;
+            case DeviceOrientation.Portrait:
+                //sceneLabel.text = "Portrait";
+                LivesLabel.rectTransform.anchoredPosition = new Vector3(246.0f,-196.0f,0f);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector3(-364.0f,-196.0f,0f);
+                break;
+            case DeviceOrientation.Unknown:
+                //sceneLabel.text = "Unknown";
+                break;
+
+        }
+    }
+}
